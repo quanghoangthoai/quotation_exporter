28f24a7da6fea51149ac1b4cf083f1eb83f130c6
quanghoangthoai <48936004+quanghoangthoai@users.noreply.github.com>
2025-04-10 13:01:19 +0700

    Create hooks.py

---
+++ hooks.py
@@ -0,0 +1,8 @@
+# quotation_exporter/hooks.py
+
+app_name = "quotation_exporter"
+app_title = "Quotation Exporter"
+app_publisher = "Your Name"
+app_description = "Export quotations to Excel"
+app_email = "you@example.com"
+app_license = "MIT"
